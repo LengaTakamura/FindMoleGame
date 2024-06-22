f31ed94e8982a83779dade20349036cae316d064
LENGA_TAKAMURA <kotodaman55324678@gmail.com>
2024-06-22 16:47:06 +0900

    [Add] ルール説明シーン、ランキングシーンの作成　UIの微調整　

---
+++ Assets/scripts/LoadRule.cs
@@ -0,0 +1,13 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadRule : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("Rule");
+    }
+
+}
+ 
